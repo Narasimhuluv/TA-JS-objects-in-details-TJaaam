993e8d05b1756b1578a776cacc1d4ee6804ae72f
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-05-31 12:57:38 +0530

    added files

--- block-BJaahu/code/index.js
+++ block-BJaahu/code/index.js
@@ -1,5 +1,36 @@
 // Prototypal pattern of object creation (Put methods inside an object and create object using Object.create)
 function Question(title, options, correctAnswerIndex) {
+  let data = Object.create(QuestionMethods);
+  data.title = title;
+  data.options = options;
+  data.correctAnswerIndex = correctAnswerIndex;
+  return data;
+}
+
+QuestionMethods = {
+  isAnswerCorrect(index) {
+    return index === this.correctAnswerIndex;
+  },
+  getCorrectAnswer() {
+    return this.options[this.correctAnswerIndex];
+  },
+};
+
+let firstData = Question(
+  "Where is the capital of Jordan",
+  ["Tashkent", "Amaan", "Kuwait City", "Nairobi"],
+  1
+);
+
+let secondData = Question(
+  "Where is the capital of Jordan",
+  ["Tashkent", "Amaan", "Kuwait City", "Nairobi"],
+  2
+);
+
+//////////////////////////////////////////
+
+function Question(title, options, correctAnswerIndex) {
   let data = Object.create(Question.prototype);
   data.title = title;
   data.options = options;
@@ -32,10 +63,10 @@ let secondData = Question(
 //Pseudoclassical Pattern (Put methods inside F.prototype and use `new` to call function)
 
 function Question(title, options, correctAnswerIndex) {
-  let data = Object.create(Question.prototype);
-  data.title = title;
-  data.options = options;
-  data.correctAnswerIndex = correctAnswerIndex;
+  //   let data = Object.create(Question.prototype);
+  this.title = title;
+  this.options = options;
+  this.correctAnswerIndex = correctAnswerIndex;
 }
 
 Question.prototype = {
